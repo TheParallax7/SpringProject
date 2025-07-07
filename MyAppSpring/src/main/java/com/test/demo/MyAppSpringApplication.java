5935c3d4c8e5cd7089375e80691d9aaee4435614
2270214 <tripathyprasad.7@gmail.com>
2025-07-07 08:21:08 +0530

    My First Commit

---
+++ MyAppSpring/src/main/java/com/test/demo/MyAppSpringApplication.java
@@ -0,0 +1,14 @@
+package com.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppSpringApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
