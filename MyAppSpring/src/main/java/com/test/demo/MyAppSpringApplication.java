6c2b3a6c1e3eab11427fb30361abbd28dec099cf
KIIT <tripathyprasad.7@gmail.com>
2025-07-07 08:52:48 +0530

    Third commit

--- MyAppSpring/src/main/java/com/test/demo/MyAppSpringApplication.java
+++ MyAppSpring/src/main/java/com/test/demo/MyAppSpringApplication.java
@@ -9,6 +9,7 @@ public class MyAppSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppSpringApplication.class, args);
 		System.out.println("Hello world");
+		System.out.println("Hello wold 1111");
 		System.out.println("Modified statement for git commit check");
 	}
 
